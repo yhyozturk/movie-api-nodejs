fcdcd5c063cb65143d098792d9f38616bb5ba3ed
yhyozturk <yahyaa.ozturrk@gmail.com>
2020-10-27 16:14:42 +0300

    added some routes before register endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -7,7 +7,20 @@ const Movie = require('../models/Movie');
 
 //Tüm filmleri getirir.
 router.get('/', (req, res) => {
-  const promise = Movie.find({});
+  const promise = Movie.aggregate([
+    {
+      $lookup: {
+        from: 'directors',
+        localField: 'director_id',
+        foreignField: '_id',
+        as: 'director',
+      },
+    },
+    {
+      $unwind: '$director',
+    },
+    
+  ]);
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
