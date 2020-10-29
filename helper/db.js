eb274c8d34dc4a76f221c1cf334ecd6faa5b28c1
yhyozturk <yahyaa.ozturrk@gmail.com>
2020-10-29 18:23:33 +0300

    still heroku and mongodb problem

--- helper/db.js
+++ helper/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://try-movie-api:deneme1578++@cluster0.jl2ao.mongodb.net/movie-apiDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    mongoose.connection.on('open', () => {
-        console.log('MongoDB: Connected');
-    });
-    mongoose.connection.on('error', (err) => {
-        console.log('MongoDB: Error', err);
-    });
+    mongoose.connect('mongodb+srv://try-movie-api:deneme1578++@cluster0.jl2ao.mongodb.net/movie-apiDB?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+            console.log('MongoDB: Connected');
+        }).catch((err) => {
+            console.log('MongoDB: Error', err);
+        });
 
     mongoose.Promise = global.Promise;
 };
