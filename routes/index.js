67a72445948174270dadf88822c00ae76927ba76
yhyozturk <yahyaa.ozturrk@gmail.com>
2020-10-29 12:52:49 +0300

    Index.js text changes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Yahya\'s movie api' });
 });
 
 router.post('/register', (req, res, next) => {
